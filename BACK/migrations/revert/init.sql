eb1716ba1232fcc58cfe96c91ee0db04c96b1e27
kertesz-corentin <kertesz.corentin@hotmail.com>
2022-02-26 11:15:56 +0100

    build: add init.sql revert

--- BACK/migrations/revert/init.sql
+++ BACK/migrations/revert/init.sql
@@ -2,6 +2,24 @@
 
 BEGIN;
 
-DROP TABLE "test";
+DROP TABLE "article";
+
+DROP TABLE "reference_to_category";
+
+DROP TABLE "reference_to_image";
+
+DROP TABLE "image";
+
+DROP TABLE "reference";
+
+DROP TABLE "category";
+
+DROP TABLE "booking";
+
+DROP TABLE "permanency";
+
+DROP TABLE "user";
+
+DROP TABLE "role";
 
 COMMIT;
