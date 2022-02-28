673c0267cd86305c185717d879b5d77a14b29402
Thibaut Lallement <90904352+monsieurtyb@users.noreply.github.com>
2022-02-28 14:59:10 +0100

    headerv1

--- FRONT/src/components/App/App.js
+++ FRONT/src/components/App/App.js
@@ -1,21 +1,13 @@
 
 import './App.scss';
+import Header from '../Header/Header';
+import { NavLink } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header/>
       </header>
     </div>
   );
--- FRONT/src/components/Header/Header.jsx
+++ FRONT/src/components/Header/Header.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { Route,
+    NavLink,
+    BrowserRouter as Router,
+    Switch
+} from 'react-router-dom'
 import './header.scss';
+import logo from '../Header/logo-lud.jpg'
 
 const Header = ({className, ...rest}) => {
    return (
        <header
             className={classnames('header', className)}
-            {...rest}
-         >
-
+            {...rest}>
+            <img className='App-logo' src={logo} alt="Logo" />
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>Notre association</li>
+                    <li>Matériathèque</li>
+                    <li>Infos pratique</li>
+                    <li>Liens utiles</li>
+                </ul>
+            {/* <NavLink to="/Accueil">Accueil
+            </NavLink> */}
+            </nav>
         </header>
    );
 };
