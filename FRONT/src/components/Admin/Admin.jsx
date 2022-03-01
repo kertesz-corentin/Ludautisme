c6993c74521fd8d8323ceb740654c1e043072443
Xavier-LEPLATRE-Promo-Y <xavier.leplatre@laposte.net>
2022-03-01 10:37:20 +0100

    feat: add isLogged prop

--- FRONT/src/components/Admin/Admin.jsx
+++ FRONT/src/components/Admin/Admin.jsx
@@ -2,23 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import MenuAdmin from './MenuAdmin/MenuAdmin';
+import LoginAdmin from './LoginAdmin/LoginAdmin';
 import './admin.scss';
 
-const Admin = ({className, ...rest}) => {
+const Admin = ({isLogged, className, ...rest}) => {
    return (
        <div
             className={classnames('admin', className)}
             {...rest}
          >
-        <MenuAdmin />
+        {!isLogged && (
+            <LoginAdmin />
+        )}
+
+        {isLogged && (
+            <MenuAdmin />
+        )}
         </div>
    );
 };
 
 Admin.propTypes = {
     className: PropTypes.string,
+    isLogged: PropTypes.bool,
 };
 Admin.defaultProps = {
     className: '',
+    isLogged: false,
+
 };
 export default React.memo(Admin);
