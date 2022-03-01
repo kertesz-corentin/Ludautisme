f703a86e7afb619067509f4be9ceb2a8b0a30d82
Xavier-LEPLATRE-Promo-Y <xavier.leplatre@laposte.net>
2022-03-01 17:45:21 +0100

    feat: add admin features

--- FRONT/src/components/App/App.js
+++ FRONT/src/components/App/App.js
@@ -2,11 +2,16 @@
 import HomePage from '../HomePage/HomePage';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import HomeUser from '../User/UserMyAccount/UserMyAccount';
 import './App.scss';
 import {Routes, Route} from 'react-router-dom';
 import UserMyAccount from '../User/UserMyAccount/UserMyAccount';
 import UserBookings from '../User/UserBookings/UserBookings';
+import AdminPage from '../Admin/AdminPage/AdminPage';
+import AdminHome from '../Admin/AdminHome/AdminHome';
+import AdminUsers from '../Admin/AdminUsers/AdminUsers';
+import AdminLogin from '../Admin/AdminLogin/AdminLogin';
+import AdminBookings from '../Admin/AdminBookings/AdminBookings';
+import AdminReferences from '../Admin/AdminReferences/AdminReferences';
 
 
 
@@ -22,18 +27,18 @@ function App() {
             {/* <Route path = "/usefulllinks" element = {<UsefullLinks />}></Route> */}
             {/* <Route path = "/shop" element = {<Shop />}></Route> */}
             {/* <Route path = "/admin/categorie" element = {<AdminCategory />}></Route> */}
-            {/* <Route path = "/admin" element = {<Admin />}></Route> */}
-            {/* <Route path = "/admin/users" element = {<AdminUsers/>}></Route> */}
+            <Route path = "/admin/home" element = {<AdminPage><AdminHome /></AdminPage>}></Route>
+            <Route path = "/admin/users" element = {<AdminPage><AdminUsers /></AdminPage>}></Route>
             {/* <Route path = "/admin/users/:id" element = {<AdminUser/>}></Route> */}
-            {/* <Route path = "/admin/references" element = {<AdminReferences />}></Route> */}
+            <Route path = "/admin/references" element = {<AdminPage><AdminReferences /></AdminPage>}></Route>
             {/* <Route path = "/admin/references/:id" element = {<AdminReference />}></Route> */}
             {/* <Route path = "/admin/references/:id/articles" element = {<AdminArticles />}></Route> */}
             {/* <Route path = "/admin/categories" element = {<AdminCategories />}></Route> */}
-            {/* <Route path = "/admin/bookings" element = {<AdminBookings />}></Route> */}
+            <Route path = "/admin/bookings" element = {<AdminPage><AdminBookings /></AdminPage>}></Route>
             {/* <Route path = "/admin/logout" element = {<AdminLogout />}></Route> */}
-            {/* <Route path = "/admin/login" element = {<AdminLogin />}></Route> */}
-                <Route path = "/user/account" element = {<UserMyAccount />}></Route>
-                <Route path = "/user/bookings" element = {<UserBookings/>}></Route>
+            <Route path = "/admin/login" element = {<AdminLogin />}></Route>
+            <Route path = "/user/account" element = {<UserMyAccount />}></Route>
+            <Route path = "/user/bookings" element = {<UserBookings/>}></Route>
             {/* <Route path = "/user/booking/active" element = {<UserBookingActive />}></Route> */}
             {/* <Route path = "/user/articles" element = {<MaterialLibrary />}></Route> */}
             {/* <Route path = "*" element = {<Error />}></Route>  */}
