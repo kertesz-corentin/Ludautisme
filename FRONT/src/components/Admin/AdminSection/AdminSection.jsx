4603ad42c0feba16fd0ac0ca01df99ea8da21e96
Xavier-LEPLATRE-Promo-Y <xavier.leplatre@laposte.net>
2022-03-02 15:19:26 +0100

    style: format dataGrid

--- FRONT/src/components/Admin/AdminSection/AdminSection.jsx
+++ FRONT/src/components/Admin/AdminSection/AdminSection.jsx
@@ -1,32 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
 import './adminsection.scss';
 
-<<<<<<< HEAD
-const AdminSection = ({ title, rows }) => {
+const AdminSection = ({ title, rows, columns }) => {
+    const showModal = () => {
+        console.log('showModal');
+    };
 
-    const columns = [
-        {field: 'id', headerName: 'ID'},
-        {field: 'name', headerName: 'Prenom'},
-        {field: 'email', headerName: 'Email'},
-        {field: 'phone', headerName: 'Telephone'}
-    ]
-
-=======
-const AdminSection = ({ title }) => {
-    const columns = [
-        {field: 'id', headerName: 'ID'},
-        {field: 'firstname', headerName: 'First Name', editable: true},
-        {field: 'email', headerName: 'Email', editable: true},
-    ];
-
-    const rows = [
-        {id: 1, firstname: 'Toto', email:'toto@email.com'},
-        {id: 2, firstname: 'Titi', email:'titi@email.com'},
-        {id: 3, firstname: 'Tata', email:'tata@email.com'}
-    ]
->>>>>>> 39ab1e53b80b914b9c858d402fdc68372c47155e
     return (
         <section className='section'>
             <div className="section-element">
@@ -35,26 +16,19 @@ const AdminSection = ({ title }) => {
             <div className="section-element">
                 <button className="section-element-add">Ajouter {title}</button>
             </div>
-<<<<<<< HEAD
             <div className="section-element">
-=======
-            <div class="section-element">
->>>>>>> 39ab1e53b80b914b9c858d402fdc68372c47155e
                 <div className="section-element-grid" style={{ height: 400, width: '100%'}}>
                     <DataGrid
                         rows={rows}
                         columns={columns}
-<<<<<<< HEAD
                         pageSize={5}
                         rowsPerPageOptions={[5]}
                         SelectionOnClick
-
-=======
-                        pageSize={15}
-                        rowsPerPageOptions={[15]}
-                        checkboxSelection
-                        disableSelectionOnClick
->>>>>>> 39ab1e53b80b914b9c858d402fdc68372c47155e
+                        editMode="cell"
+                        onRowClick={showModal}
+                        components={{
+                            Toolbar: GridToolbar,
+                        }}
                     />
                 </div>
             </div>
--- FRONT/src/components/Admin/AdminUsers/AdminUsers.jsx
+++ FRONT/src/components/Admin/AdminUsers/AdminUsers.jsx
@@ -10,7 +10,7 @@ const AdminUsers = ({className, ...rest}) => {
 
     const getUsers = async () => {
         try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            const response = await axios.get('http://localhost:3001/api/admin/users');
             const data = await response.data;
             console.log('response', response);
             setUsers(data);
@@ -25,20 +25,59 @@ const AdminUsers = ({className, ...rest}) => {
         getUsers();
     }, []);
 
-    const rowData = users.map(user => {
-        return {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            phone: user.phone,
+    const columnsData = [
+        {field: 'member_number', headerName: 'n°adhérent', width: 100},
+        {field: 'first_name', headerName: 'Prenom', editable: true, width: 125},
+        {field: 'last_name', headerName: 'Nom', editable: true, width: 125},
+        {field: 'email', headerName: 'Email', editable: true, width: 200},
+        {field: 'phone', headerName: 'Telephone', editable: true, width: 200},
+        {field: 'adress_number', headerName: 'n°', editable: true, width: 50},
+        {field: 'adress_street', headerName: 'Rue', editable: true, width: 200},
+        {field: 'adress_zipcode', headerName: 'Code Postal', editable: true, width: 125},
+        {field: 'adress_city', headerName: 'Ville', editable: true, width: 200},
+        {field: 'created_at', headerName: 'Membre depuis', editable: true, width: 100},
+    ]
+
+    const rows = [
+        {
+            id: 1,
+            member_number: 1,
+            first_name: 'Xavier',
+            last_name: 'LEPLATRE',
+            email: 'test@mail.com',
+            phone: '06 xx xx xx xx',
+            adress_number: '1',
+            adress_street: 'rue de la Liberté',
+            adress_zipcode: '75000',
+            adress_city: 'PARIS',
+            created_at: '01-01-2022',
         }
-    })
+    ]
+
+    // const rowData = users.map(user => {
+    //     return {
+    //         id: user.id,
+    //         phone: user.phone,
+    //         first_name: user.first_name,
+    //         last_name: user.last_name,
+    //         email: user.email,
+    //         adress_number: user.adress_number,
+    //         adress_street: user.adress_street,
+    //         adress_zipcode: user.adress_zipcode,
+    //         adress_city: user.adress_city,
+    //         cotisation_status: user.cotisation_status,
+    //         cotisation_expiration : user.cotisation_expiration,
+    //         caution_status: user.caution_status,
+    //         caution_expiration : user.caution_expiration,
+    //         created_at: user.created_at,
+    //     }
+    // })
     return (
         <div
                 className={classnames('adminusers', className)}
                 {...rest}
             >
-                <AdminSection title="Adhérent" rows={rowData} />
+                <AdminSection title="Adhérent" rows={rows} columns={columnsData} />
             </div>
     );
 };
