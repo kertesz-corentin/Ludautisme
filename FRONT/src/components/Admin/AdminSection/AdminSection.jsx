a1987cf4041a7997f16d52d357be070d41a95d27
Xavier-LEPLATRE-Promo-Y <xavier.leplatre@laposte.net>
2022-03-02 14:20:01 +0100

    feat: create datagrid for api

--- FRONT/src/components/Admin/AdminSection/AdminSection.jsx
+++ FRONT/src/components/Admin/AdminSection/AdminSection.jsx
@@ -3,35 +3,32 @@ import PropTypes from 'prop-types';
 import { DataGrid } from '@mui/x-data-grid';
 import './adminsection.scss';
 
-const AdminSection = ({ title }) => {
+const AdminSection = ({ title, rows }) => {
+
     const columns = [
         {field: 'id', headerName: 'ID'},
-        {field: 'firstname', headerName: 'First Name', editable: true},
-        {field: 'email', headerName: 'Email', editable: true},
-    ];
-
-    const rows = [
-        {id: 1, firstname: 'Toto', email:'toto@email.com'},
-        {id: 2, firstname: 'Titi', email:'titi@email.com'},
-        {id: 3, firstname: 'Tata', email:'tata@email.com'}
+        {field: 'name', headerName: 'Prenom'},
+        {field: 'email', headerName: 'Email'},
+        {field: 'phone', headerName: 'Telephone'}
     ]
+
     return (
         <section className='section'>
-            <div class="section-element">
+            <div className="section-element">
                 <h1 className="section-element-title">Tableau de bord - {title}</h1>
             </div>
-            <div class="section-element">
+            <div className="section-element">
                 <button className="section-element-add">Ajouter {title}</button>
             </div>
-            <div class="section-element">
+            <div className="section-element">
                 <div className="section-element-grid" style={{ height: 400, width: '100%'}}>
                     <DataGrid
                         rows={rows}
                         columns={columns}
-                        pageSize={15}
-                        rowsPerPageOptions={[15]}
-                        checkboxSelection
-                        disableSelectionOnClick
+                        pageSize={5}
+                        rowsPerPageOptions={[5]}
+                        SelectionOnClick
+
                     />
                 </div>
             </div>
--- FRONT/src/components/Admin/AdminUsers/AdminUsers.jsx
+++ FRONT/src/components/Admin/AdminUsers/AdminUsers.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import AdminSection from '../AdminSection/AdminSection';
+import axios from 'axios';
 import './adminusers.scss';
 
 const AdminUsers = ({className, ...rest}) => {
+    const [users, setUsers] = useState([]);
 
-   return (
-       <div
-            className={classnames('adminusers', className)}
-            {...rest}
-         >
-            <AdminSection title="Adhérent" />
-        </div>
-   );
+    const getUsers = async () => {
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+            const data = await response.data;
+            console.log('response', response);
+            setUsers(data);
+
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const rowData = users.map(user => {
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+        }
+    })
+    return (
+        <div
+                className={classnames('adminusers', className)}
+                {...rest}
+            >
+                <AdminSection title="Adhérent" rows={rowData} />
+            </div>
+    );
 };
 
 AdminUsers.propTypes = {
