c603dca95c7048800d3942587d903d3c3b6a88ee
kertesz-corentin <kertesz.corentin@hotmail.com>
2022-03-03 08:51:36 +0100

    refactor: use index of customer router

--- BACK/app/routes/api/index.js
+++ BACK/app/routes/api/index.js
@@ -3,8 +3,7 @@ const ApiError = require('../../errors/apiError');
 
 const loginRoutes = require('./login/index');
 const adminUserRoutes = require('./admin/users');
-const customerReferenceRouter = require('./customer/reference');
-const userRoute = require('./customer/user');
+const userRoute = require('./customer/index');
 
 const authAdmin = require('../../middleware/admin');
 const authUser = require('../../middleware/user');
@@ -15,9 +14,8 @@ const router = express.Router();
 router.use('/login', loginRoutes);
 // !router.all('/',apiController.home);
 
-router.use('/user/articles', authUser, customerReferenceRouter);
 router.use('/admin/users', authAdmin, adminUserRoutes);
-router.use('/user', userRoute);
+router.use('/user', authUser, userRoute);
 // !router.use('/customer', customerRouter);
 
 router.use(() => {
