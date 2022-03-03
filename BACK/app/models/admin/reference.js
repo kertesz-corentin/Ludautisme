5659ddf44c4adae24baed50c09e2dce3c0dde728
kertesz-corentin <kertesz.corentin@hotmail.com>
2022-03-03 15:28:15 +0100

    feat: admin get all ref

--- BACK/app/models/admin/reference.js
+++ BACK/app/models/admin/reference.js
@@ -9,14 +9,25 @@ module.exports = {
         r.description,
         r.valorisation,
         cat.name AS mainCategory,
-        json_agg("category"."name") AS tag
+        json_agg(json_build_object('tag', "category"."name")) AS tag
         FROM "reference" AS r
         JOIN "category" AS cat ON r."id_category" = cat."id"
         JOIN "reference_to_category" AS rtc ON rtc."id_ref" = r."id"
         JOIN "category" ON rtc."id_category" = "category"."id"
-        GROUP BY r.name, r.description, r.valorisation, r.id, cat.name, "category"."name"
+        GROUP BY r.name, r.description, r.valorisation, r.id, cat.name
         `);
-        console.log(result);
         return result.rows;
+        /* SELECT
+        r.id,
+        r.name,
+        r.description,
+        r.valorisation,
+        cat.name AS mainCategory,
+        json_object_agg('tag', "category"."name")
+        FROM "reference" AS r
+        JOIN "category" AS cat ON r."id_category" = cat."id"
+        JOIN "reference_to_category" AS rtc ON rtc."id_ref" = r."id"
+        JOIN "category" ON rtc."id_category" = "category"."id"
+        GROUP BY  r.name, r.description, r.valorisation, r.id, "category"."name", cat.name */
     },
 };
