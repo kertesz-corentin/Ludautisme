0a4fb4b7b5542fded499e19f2f56d01387db45ae
Xavier-LEPLATRE-Promo-Y <xavier.leplatre@laposte.net>
2022-03-03 15:38:53 +0100

    feat: test response api login admin

--- FRONT/src/requests/requestsAdmin/loginAdmin.js
+++ FRONT/src/requests/requestsAdmin/loginAdmin.js
@@ -1,13 +1,22 @@
 import api from '../index';
 
+/**
+ *
+ * @param {string} email : user email
+ * @param {string} password : user password
+ * @returns
+ */
 export async function requestLoginAdmin(email, password) {
     try {
         const response = await api.post('/login/admin', {
             email, password
         });
+        console.log('responseApi :', response);
         return response
     }
     catch (err) {
         return err.response;
     }
 }
+
+
