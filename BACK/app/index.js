419e9b6a53a9be5c8b99951464a6311ad8ac4629
Arnocaster <arnaud.peybernes@gmail.com>
2022-03-03 08:42:58 +0100

    ci: configure dev and heroku environment*

--- BACK/app/index.js
+++ BACK/app/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes');
-
 const app = express();
 
+//Serve React app
+const { dirname } = require('path');
+const appDir = dirname(require.main.filename);
+const buildPath = `${appDir}/../FRONT/build`;
+//END Serve React app
 require('./helpers/apiDoc')(app);
 
-app.use(express.static('../FRONT/build/public'));
-
+app.use('/',express.static(buildPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(process.env.CORS_DOMAINS ?? '*'));
