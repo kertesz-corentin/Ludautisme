01cc158debaeeea91821aac4ff52a5e579e901fd
Arnocaster <arnaud.peybernes@gmail.com>
2022-03-03 07:18:18 +0100

    fix: node-dev start deleted

--- BACK/app/routes/index.js
+++ BACK/app/routes/index.js
@@ -7,7 +7,6 @@ const { errorHandler } = require('../helpers/errorHandler');
 const router = express.Router();
 
 router.use('/api', apiRouter);
-// !router.use('/', websiteRouter);
 
 router.use((err, _, response, next) => {
     errorHandler(err, response, next);
