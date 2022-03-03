2def95bdba12cb93052377466333f7d00f430057
RomainPerdriat <romainperdriat@gmail.com>
2022-03-03 15:57:45 +0100

    feat: authlogin bug

--- FRONT/src/requests/requestsUser/login.js
+++ FRONT/src/requests/requestsUser/login.js
@@ -4,11 +4,15 @@ import api from '../index';
 export async function requestLoginUser(email, password) {
     try {
         const response = await api.post('/login/user', {
-             email, password,
+             email, password
         });
+        console.log(`Voila la réponse de l'api`, response);
         return response
+
     }
     catch (err) {
+        console.log(`Voici l'erreur provenant de l 'api`, err.response)
         return err.response;
+
     }
 }
