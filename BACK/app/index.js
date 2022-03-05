b06fc9c40538b1248408eec6d80db041a723302a
Arnaud Peybernes <arnaud.peybernes@gmail.com>
2022-03-05 08:10:59 +0100

    feat: change BACK router for deployment

--- BACK/app/index.js
+++ BACK/app/index.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const router = require('./routes');
+
 const app = express();
 
-//Serve React app
-const { dirname } = require('path');
-const appDir = dirname(require.main.filename);
-const buildPath = `${appDir}/../FRONT/build`;
-app.use('/', express.static(buildPath));
-app.use('/admin', express.static(buildPath));
-//END Serve React app
 if (process.env.NODE_ENV !== 'production') {
     require('./helpers/apiDoc')(app);
 }
-
-app.use(express.static('../FRONT/build/public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
