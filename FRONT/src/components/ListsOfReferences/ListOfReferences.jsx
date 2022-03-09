9dee920de56b56d588111c6e13fc75b0b9164f9c
RomainPerdriat <romainperdriat@gmail.com>
2022-03-09 14:33:33 +0100

    feat: reference ok, push before add modal

--- FRONT/src/components/ListsOfReferences/ListOfReferences.jsx
+++ FRONT/src/components/ListsOfReferences/ListOfReferences.jsx
@@ -3,17 +3,32 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import './listofreferences.scss';
 import Reference from '../Reference/Reference';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 const ListOfReferences = ({className, ...rest}) => {
-   return (
-       <div
-            className={classnames('listofreferences', className)}
-            {...rest}
-         >
-            Voila la liste des réferences
+    return (
+        <React.Fragment>
+          <CssBaseline />
+          <Container maxWidth="sl">
+            <Stack spacing={2}>
+                <Pagination count={10} color="primary" />
+            </Stack>
+            <Box className= "lisofreferences" />
+            Voici la list des réferences
             <Reference/>
-        </div>
-   );
+            <Reference/>
+            <Reference/>
+            <Reference/>
+            <Stack spacing={2}>
+                <Pagination count={10} color="primary" />
+            </Stack>
+          </Container>
+        </React.Fragment>
+      );
 };
 
 ListOfReferences.propTypes = {
--- FRONT/src/components/Reference/Reference.jsx
+++ FRONT/src/components/Reference/Reference.jsx
@@ -2,15 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import './reference.scss';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 const Reference = ({className, ...rest}) => {
    return (
-       <div
-            className={classnames('reference', className)}
-            {...rest}
-         >
-            Voici une reference
-        </div>
+    <Card sx={{ maxWidth: 345 }}>
+    <CardActionArea>
+      <CardMedia
+        component="img"
+        height="140"
+        image="../../public/legosfix.png"
+        alt="image about the article"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Réference 1
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Here you can read a description of the article
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+  </Card>
    );
 };
 
