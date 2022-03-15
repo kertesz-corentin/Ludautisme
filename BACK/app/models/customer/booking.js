1404abc75a15bb286334f4fa3d9853b4871d9cf8
Arnaud Peybernes <arnaud.peybernes@gmail.com>
2022-03-15 09:40:43 +0100

    fix: booking customer BACK + booking customer list  FRONT

--- BACK/app/models/customer/booking.js
+++ BACK/app/models/customer/booking.js
@@ -2,8 +2,8 @@
 const sqlHandler = require('../../helpers/sqlHandler');
 
 /**
- * @typedef {object} paramsBooking
- * @property {number} refIds - Array of refs
+ * @typedef {object} paramAddBookingByRef
+ * @property {array<number>} refIds - Array of refs
  */
 
 module.exports = {
@@ -42,6 +42,64 @@ module.exports = {
         const placeholders = [id];
         const result = await sqlHandler(query, placeholders);
         return result.rows;
+    },
+    async findFiltered(arr) {
+        let query = `
+        WITH booking_full AS(
+			SELECT
+            b.id,
+            b.delivered,
+            b.closed,
+            b.id_permanency,
+            "user"."id" AS id_user,
+        	"user"."member_number" AS member_number,
+            "user"."first_name",
+            "user"."last_name",
+        	"user"."email",
+            "perm"."perm_date" AS date_permanency,
+        	"perm"."next_id" AS return_id_permanency,
+        	"perm"."next_date" AS return_date_permanency,
+            "perm"."active" AS active_permanency,
+        	(perm."next_date" > CURRENT_DATE) AND (b.delivered = true ) AND (b.closed = false) AS overdue,
+            json_agg(json_build_object (
+                'id', borrowed."id",
+                'number', borrowed."number",
+                'available', borrowed."available",
+                'archived', borrowed."archived",
+				'id_ref', "reference"."id",
+				'name_ref', "reference"."name",
+				'description_ref', "reference"."description"
+                )) AS "borrowed_articles"
+        FROM "booking" AS b
+	    INNER JOIN "full_perm" AS perm ON "perm"."id" = "b"."id_permanency"
+        INNER JOIN "user" ON "user"."id"="b"."id_user"
+        LEFT JOIN "article_to_booking" AS borrowed_ar_to_book ON "b"."id" = "borrowed_ar_to_book"."id_booking"
+        LEFT JOIN "article" AS borrowed ON "borrowed_ar_to_book"."id_article" = "borrowed"."id"
+        INNER JOIN "reference" ON "reference"."id"="borrowed"."id_ref"
+		GROUP BY b.id, "user"."id",
+                "date_permanency","return_date_permanency","return_id_permanency","active_permanency"
+        )
+        SELECT *
+        FROM booking_full
+        WHERE `;
+        const placeholders = [];
+        console.log(arr);
+        try {
+            arr.forEach((filter, index) => {
+                let prop = Object.keys(filter)[0];
+                console.log(prop);
+                placeholders.push(filter[prop]);
+                if (index !== arr.length - 1) {
+                    query += `"${prop}"=$${index + 1} AND `;
+                } else {
+                    query += `"${prop}"=$${index + 1} `;
+                }
+            });
+            const result = await sqlHandler(query, placeholders);
+            return result.rows;
+        } catch (err) {
+            console.error(err);
+        }
     },
     async findHistory(id) {
         console.log(id);
@@ -131,15 +189,37 @@ module.exports = {
         const result = await sqlHandler(query, placeholders);
         return result.rows;
     },
-    async getArticlesAvailability(arr) {
+    async getRefsAvailability(arr) {
         let query = `
-       SELECT * FROM "article" WHERE "article"."id" IN (`;
+        WITH add_row_number AS (
+            SELECT
+            "reference"."id",
+            "reference"."name",
+            "article"."id" AS "article_id" ,
+            "article"."available" AS "article_available",
+            "article"."archived" AS "article_archived",
+            ROW_NUMBER() OVER(PARTITION BY "reference"."id") AS row_number
+            FROM "reference"
+            INNER JOIN "article" ON "reference"."id" = "article"."id_ref"
+            WHERE "reference"."id" IN (`;
         const placeholders = [];
         arr.forEach((articleId, index) => {
             query += `$${index + 1}`;
-            query += (index < arr.length - 1) ? `,` : `);`;
+            query += (index < arr.length - 1) ? `,` : `)`;
             placeholders.push(articleId);
         });
+        query += `
+        )
+        SELECT
+            "id",
+            "name",
+            "article_id" ,
+            "article_available",
+            "article_archived"
+        FROM add_row_number
+        WHERE ("article_available"='true' AND "article_archived"='false' AND row_number=1)
+            OR ("article_available"='false' AND row_number=1)
+        `;
         const result = await sqlHandler(query, placeholders);
         return result.rows;
     },
--- FRONT/src/components/User/UserBookings/UserBookings.jsx
+++ FRONT/src/components/User/UserBookings/UserBookings.jsx
@@ -10,7 +10,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import api from '../../../requests';
-import ListOfArticles from '../../ListsOfArticles/ListOfArticles';
+import ListOfReferences from '../../ListsOfReferences/ListOfReferences';
 
 
 
@@ -20,6 +20,7 @@ import ListOfArticles from '../../ListsOfArticles/ListOfArticles';
 const UserBookings = ({className, ...rest}) => {
     const [bookings, setBookings] = useState([]);
     const [activeBooking, setActiveBooking] = useState([]);
+    const [nextBooking, setNextBooking] = useState([]);
     const [idUser,setIdUser] = useState(0);
 
 
@@ -27,13 +28,44 @@ const UserBookings = ({className, ...rest}) => {
         try {
             const { id } = await JSON.parse(localStorage.getItem('user'));
             const response = await api.get(`/customer/booking/history/${id}`);
-            const data = await response.data;
+            let data = await response.data;
+            console.log("data",data);
+            data = data.map((booking) => {
+                booking['references'] = booking.articles.map((article) => {
+                    return {
+                            id: article.id_ref,
+                            name: article.name_ref,
+                            description: article.description_ref,
+                            art_id: article.id,
+                            picture: [
+                                {
+                                    id:article.id_picture_ref,
+                                    url:article.url_picture_ref,
+                                    text:article.text_picture_ref,
+                                }
+                            ],
+                        }
+                });
+                delete booking.articles;
+                return booking
+            });
             setBookings(data);
+            console.log(data);
+
+            //Next Permanency
+            const nextFilter = (data) ?
+            await data.filter((booking)=> booking.is_next_permanency)
+            : [];
+            setNextBooking(nextFilter);
+
+            //Active permanency
             const activeFilter = (data) ?
             await data.filter((booking)=> booking.active_permanency)
-            :
-            [];
+            : [];
             setActiveBooking(activeFilter);
+            //Old Bookings
+
+
         }
         catch (err) {
             console.error (err);
@@ -46,27 +78,33 @@ const UserBookings = ({className, ...rest}) => {
 
    return (
     <div> MES RESERVATIONS
-        <Permanency/>
             <div
                     className={classnames('home-user', className)}
                     {...rest}
                 >
                     <MenuUser/>
-                    <div className = "list">
-                        <h2>En cours</h2>
-                        <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                            { (activeBooking[0]) ?
-                                <ListOfArticles articles= {activeBooking[0].articles}/>
+                    <Box className = "list" sx={{ bgcolor: 'background.paper' }}>
+                        <h2>Réservation prochaine permanence</h2>
+                        <Permanency/>
+                        <Box sx={{ bgcolor: 'background.paper' }}>
+                            { (nextBooking) ?
+                                <ListOfReferences references= {nextBooking[0].references}/>
                             :
                             <div>WAIT</div>}
-                            {console.log('test',activeBooking[0])}
+                            {console.log('next',nextBooking)}
                         </Box>
-                    </div>
-                    <div className = "list">
+                        <h2>Emprunt en cours</h2>
+                        <Box sx={{ bgcolor: 'background.paper' }}>
+                            { (activeBooking[0]) ?
+                                <ListOfReferences references= {activeBooking[0].references}/>
+                            :
+                            <div>WAIT</div>}
+                            {console.log('active',activeBooking[0])}
+                        </Box>
                         <h2>Historique</h2>
                         <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
                         </Box>
-                    </div>
+                    </Box>
             </div>
     </div>
 
