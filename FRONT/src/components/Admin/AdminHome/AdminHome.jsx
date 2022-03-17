1f24b1a4e84b468ef09a98277d87b6f76d6632f0
Xavier-LEPLATRE-Promo-Y <xavier.leplatre@laposte.net>
2022-03-17 17:32:00 +0100

    feat: create modify permanency date

--- FRONT/src/components/Admin/AdminHome/AdminHome.jsx
+++ FRONT/src/components/Admin/AdminHome/AdminHome.jsx
@@ -1,20 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
+// import requests
+import api from '../../../requests/index';
+
 // import react components
 import AdminPermanency from '../AdminPermanency/AdminPermanency';
+import AdminHomeCard from '../AdminHomeCard/AdminHomeCard';
 
 import './adminhome.scss';
 
 const AdminHome = ({isLogged, className, ...rest}) => {
+    const [bookings, setBookings] = useState([]);
+    const [overdueBookings, setOverdueBookings] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [references, setReferences] = useState([]);
+
+    const allBookings = async () => {
+        const response = await api.get('/admin/booking');
+        const data = await response.data;
+        if(response.status === 200){
+            setBookings(data.length);
+        }
+        else {
+            console.log(response.data)
+        }
+    }
+
+    const delayBookings = async () => {
+        const response = await api.post('/admin/booking/search', {overdue: true});
+        const data = await response.data;
+        if(response.status === 200){
+            setOverdueBookings(data.length);
+        }
+        else {
+            console.log(response.data)
+        }
+    }
+
+    const allUsers = async () => {
+        const response = await api.get('/admin/users');
+        const data = await response.data;
+        if(response.status === 200){
+            setUsers(data.length);
+        }
+        else {
+            console.log(response.data)
+        }
+    }
+
+    const allReferences = async () => {
+        const response = await api.get('/admin/references');
+        const data = await response.data;
+        if(response.status === 200){
+            setReferences(data.length);
+        }
+        else {
+            console.log(response.data)
+        }
+    }
+
+    console.log('bookings', bookings);
+
+    useEffect(() => {
+        allBookings();
+        delayBookings();
+        allUsers();
+        allReferences();
+    }, [])
+
     return (
         <div
             className={classnames('adminhome', className)}
             {...rest}
-         >
-            Accueil
-            <AdminPermanency />
+        >
+            <div className='adminhome-element'>
+                <div className="adminhome-element-item">
+                    <AdminPermanency />
+                </div>
+                <div className="adminhome-element-item">
+                    <AdminHomeCard title={'Réservations'} data={bookings} status={'en cours'} tag='booking' />
+                </div>
+                <div className="adminhome-element-item">
+                    <AdminHomeCard title={'Réservations'} data={overdueBookings} status={'en retard'} tag='booking' />
+                </div>
+                <div className="adminhome-element-item">
+                    <AdminHomeCard title={'Adhérents'} data={users} status={'inscrits'} tag='user' />
+                </div>
+                <div className="adminhome-element-item">
+                    <AdminHomeCard title={'Références'} data={references} status={'enregistrées'} tag='reference' />
+                </div>
+            </div>
         </div>
     );
 };
--- FRONT/src/components/Admin/AdminPermanency/AdminPermanency.jsx
+++ FRONT/src/components/Admin/AdminPermanency/AdminPermanency.jsx
@@ -1,47 +1,92 @@
 import * as React from 'react';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import 'date-fns'
+import frLocale from 'date-fns/locale/fr';
+
+import api from '../../../requests';
 
 // import material ui components
 import TextField from '@mui/material/TextField';
-import AdapterDateMoment from '@mui/lab/AdapterMoment';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DateTimePicker from '@mui/lab/DateTimePicker';
+import DatePicker from '@mui/lab/DatePicker';
+import { format } from 'date-fns';
 
 import './adminpermanency.scss';
+import { Button } from '@mui/material';
 
 const AdminPermanency = ({className, ...rest}) => {
-    const [beginTime, setBeginTime] = useState(new Date());
-    const [endTime, setEndTime] = useState(new Date());
+    const [date, setDate] = useState(new Date());
 
-    console.log(('time', beginTime));
+    const getActivePermanency = async () => {
+        const response = await api.get('/admin/permanency/active');
+        const activePermanency = await response.data;
+        console.log('permanence', activePermanency[0].next_date)
+        if(response.status === 200){
+            setDate(format(new Date(activePermanency[0].next_date), 'yyyy-MM-dd', {timeZone: 'Europe/Paris'}));
+        }
+        else {
+            console.log(response.data)
+        }
+    }
+
+    const handleChangeDate = (event) => {
+        setDate(format(new Date(event), 'yyyy-MM-dd', {timeZone: 'Europe/Paris'}));
+    }
+
+    const setPermanencyDate = async () => {
+        const newDate = {
+            'next_date': date
+        }
+        console.log(newDate);
+        const response = await api.patch('/admin/permanency/next', newDate);
+
+        if(response.status === 200){
+            console.log(response);
+        }
+        else {
+            console.log(response.data)
+        }
+    }
+
+    React.useEffect(() => {
+        getActivePermanency();
+    }, [setDate])
+
+    console.log('date', date);
+
     return (
         <div className="adminpermanency">
             <h2 className="adminpermanency-title">Prochaine permanence</h2>
             <div className="adminpermanency-element">
-                <LocalizationProvider dateAdapter={AdapterDateMoment}>
-                    <DateTimePicker
-                        renderInput={(props) => <TextField {...props} />}
-                        label="Date de début"
-                        value={beginTime}
-                        onChange={(newValue) => {
-                        setBeginTime(newValue);
-                        }}
+                <LocalizationProvider dateAdapter={AdapterDateFns} locale={frLocale}>
+                    <DatePicker
+                        label="Sélectionner une date"
+                        value={date}
+                        onChange={(event) => handleChangeDate(event)}
+                        renderInput={(params) => <TextField {...params} />}
                     />
                 </LocalizationProvider>
             </div>
-            <div className="adminpermanency-element">
+            <div className='adminpermanency-button'>
+                <Button
+                    variant='outlined'
+                    onClick={setPermanencyDate}
+                >
+                    modifier
+                </Button>
+            </div>
+            {/* <div className="adminpermanency-element">
                 <LocalizationProvider dateAdapter={AdapterDateMoment}>
-                    <DateTimePicker
-                        renderInput={(props) => <TextField {...props} />}
+                    <StaticDateTimePicker
+                        renderInput={(params) => <TextField {...params} />}
                         label="Date de fin"
                         value={endTime}
-                        onChange={(newValue) => {
-                        setEndTime(newValue);
-                        }}
+                        onChange={date2 => setEndTime(date2)}
                     />
                 </LocalizationProvider>
-            </div>
+            </div> */}
         </div>
     );
 };
