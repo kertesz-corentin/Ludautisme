8ac4806c51d948ac598c37911bda8f66ccad13dc
kertesz-corentin <kertesz.corentin@hotmail.com>
2022-06-05 12:00:58 +0200

    feat: contact form finish

--- BACK/app/controllers/customer/index.js
+++ BACK/app/controllers/customer/index.js
@@ -3,6 +3,7 @@ const referenceController = require('./reference');
 const categoryController = require('./category');
 const bookingController = require('./booking');
 const permanencyController = require('./permanency');
+const contactController = require('./contact');
 
 module.exports = {
     userController,
@@ -10,4 +11,5 @@ module.exports = {
     categoryController,
     bookingController,
     permanencyController,
+    contactController,
 };
--- BACK/app/routes/api/customer/index.js
+++ BACK/app/routes/api/customer/index.js
@@ -5,6 +5,7 @@ const userRouter = require('./user');
 const categoryRouter = require('./category');
 const bookingRouter = require('./booking');
 const permanencyRouter = require('./permanency');
+const contactRouter = require('./contact');
 const { ApiError } = require('../../../errors/apiError');
 
 const router = express.Router();
@@ -18,6 +19,8 @@ router.use('/category', categoryRouter);
 router.use('/booking', bookingRouter);
 
 router.use('/permanency', permanencyRouter);
+
+router.use('/contact', contactRouter);
 
 router.use(() => {
     throw new ApiError(404, 'API Route not found');
