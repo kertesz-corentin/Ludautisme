d1df88b98bb14b04a80b79705b02f113ba184c5d
Arnaud Peybernes <arnaud.peybernes@gmail.com>
2022-07-12 00:40:54 +0200

    fix: path sqitch.plan

--- BACK/app/config/db.js
+++ BACK/app/config/db.js
@@ -3,10 +3,10 @@ const { Pool } = require('pg');
 const config = {};
 // Heroku
 if (process.env.NODE_ENV !== 'dev') {
-    config.connectionString = process.env.DATABASE_URL;
-    // config.ssl = {
-    //     rejectUnauthorized: false,
-    // };
+    config.connectionString = 'postgres:// vxto8575_apey:@pey@pey12@127.0.0.1:5432/vxto8575_ludautisme';
+    config.ssl = {
+        rejectUnauthorized: false,
+    };
 } else {
     config.connectionString = process.env.DATABASE_DEV;
 }
