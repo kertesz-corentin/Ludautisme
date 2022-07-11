cf0171caa423d6a73cf13ce82fafed3e2afba229
Arnaud Peybernes <arnaud.peybernes@gmail.com>
2022-07-11 20:51:55 +0200

    fix: react route buildpath

--- BACK/app/routes/website/index.js
+++ BACK/app/routes/website/index.js
@@ -7,7 +7,8 @@ const router = express.Router();
 // Serve React app
 
 const appDir = dirname(require.main.filename);
-const buildPath = (process.env.REACT_FOLDER) ? process.env.REACT_FOLDER : `${appDir}/../FRONT/build`;
+const buildPath = (process.env.REACT_BUILD_FOLDER) ? process.env.REACT_BUILD_FOLDER : `${appDir}/../FRONT/build`;
+console.error('REACT BUILPATH', buildPath, process.env.REACT_BUILD_FOLDER);
 
 router.use('/admin', express.static(buildPath));
 router.use('/admin/*', express.static(buildPath));
