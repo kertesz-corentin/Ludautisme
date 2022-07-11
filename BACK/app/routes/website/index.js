159e7c8e98501532fa90a9740960075c5578c412
Arnaud Peybernes <arnaud.peybernes@gmail.com>
2022-07-11 20:17:45 +0200

    fix: react route buildpath

--- BACK/app/routes/website/index.js
+++ BACK/app/routes/website/index.js
@@ -6,16 +6,14 @@ const router = express.Router();
 
 // Serve React app
 
-const appDir = dirname(require.main.filename);
-const buildPath = `${appDir}/../FRONT/build`;
-
-const getBuildPath = (req, res, next) => {
+const getBuildPath = (req) => {
+    const appDir = dirname(require.main.filename);
     const fixedPathsHost = ['ludautisme.fr'];
-    console.error(fixedPathsHost.includes(req.get('host')), req.get('host'));
-    fixedPathsHost.includes(req.get('host'))
-        ? express.static('../FRONT/build')
-        : express.static(buildPath);
-    next();
+    console.error('testo', fixedPathsHost.includes(req.get('host')), req.get('host'));
+    const buildPath = (fixedPathsHost.includes(req.get('host')))
+        ? `${appDir}/../FRONT/build`
+        : '../FRONT/build';
+    express.static(buildPath);
 };
 
 router.use('/admin', getBuildPath);
