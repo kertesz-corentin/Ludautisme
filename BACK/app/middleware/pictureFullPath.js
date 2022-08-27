d32a37d0b9aa9e0844cce44306d2aa112a9c5101
kertesz-corentin <kertesz.corentin@hotmail.com>
2022-08-27 19:19:31 +0200

    fix: responsive menu

--- BACK/app/middleware/pictureFullPath.js
+++ BACK/app/middleware/pictureFullPath.js
@@ -2,8 +2,8 @@ const ApiError = require('../errors/apiError');
 
 module.exports = (req, res, next) => {
     const fullUrl = (process.env.IMAGE_PORT_REDIRECTION)
-        ? `${req.protocol}://${req.get('host').split(':')[0]}:${process.env.IMAGE_PORT_REDIRECTION}/api${process.env.IMAGE_CATALOG_FOLDER}/`
-        : `${req.protocol}://${req.get('host')}/api${process.env.IMAGE_CATALOG_FOLDER}/`;
+        ? `${req.protocol}://ludautisme.org/api${process.env.IMAGE_CATALOG_FOLDER}/`
+        : `${req.protocol}://ludautisme.org/api${process.env.IMAGE_CATALOG_FOLDER}/`;
 
     const old = res.json.bind(res);
 
--- FRONT/src/components/Front-Office/Home/Header/Header.jsx
+++ FRONT/src/components/Front-Office/Home/Header/Header.jsx
@@ -1,17 +1,19 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import Logo from '../../../../public/logo.png';
-import LoginUser from '../../User/LoginUser/LoginUser'
+import burgerMenu from '../../../../public/menu.png';
+import LoginUser from '../../User/LoginUser/LoginUser';
 import './header.scss';
 // import { NavLink } from 'react-router-dom';
 // import CartModal from '../CartModal/CartModal';
 import Cart from '../../Cart/Cart';
 import Box from '@mui/material/Box';
 import { Link } from 'react-router-dom';
-import {Button} from '@mui/material';
+import { Button } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
+import Popover from '@mui/material/Popover';
 
 
 const Header = ({
@@ -19,68 +21,115 @@ const Header = ({
     currentItemsNumber,
     currentItems,
     cartManager,
-     ...rest}) => {
+    ...rest }) => {
     const userToken = JSON.parse(localStorage.getItem('user'));
-    const [isActive,setIsActive] = useState();
+    const [isActive, setIsActive] = useState();
 
     const pages = {
-        test1:{ url: '/', display: <HomeIcon /> },
-        test2:{ url: '/about', display: 'Association' },
-        test3:{ url: '/materiallibrary', display: 'Mathériathèque' },
-        test4:{ url: '/infos', display: 'Infos pratiques' },
-        test5:{ url: '/usefullLinks', display: 'Liens utiles' },
+        test1: { url: '/', display: <HomeIcon /> },
+        test2: { url: '/about', display: 'Association' },
+        test3: { url: '/materiallibrary', display: 'Mathériathèque' },
+        test4: { url: '/infos', display: 'Infos pratiques' },
+        test5: { url: '/usefullLinks', display: 'Liens utiles' },
     };
 
     const handleActive = (event) => {
         setIsActive(event);
     };
 
-    useEffect(() => {}, [userToken])
+    const [anchorMenu, setAnchorMenu] = React.useState(null);
+
+    const handleClickMenu = (event) => {
+        setAnchorMenu(event.currentTarget);
+    };
+
+    const handleCloseMenu = () => {
+        setAnchorMenu(null);
+    };
+
+    const open = Boolean(anchorMenu);
+
+    const id = open ? 'simple-popover' : undefined;
+
+    useEffect(() => { }, [userToken])
 
     return (
-       <header
+        <header
             className={classnames('header', className)}
             id="myHeader"
             {...rest}>
-        <Box className="header-content">
-            <div className="header-logo">
-                <img src={Logo} className="header-logo-img" alt="Logo" />
-            </div>
+            <Box className="header-content">
+                <div className="header-logo">
+                    <img src={Logo} className="header-logo-img" alt="Logo" />
+                </div>
 
-            <nav>
-                <label className="toggleheader" for="toggleheader">
-                    <MenuIcon/>
-                </label>
-                <input type="checkbox" id="toggleheader" />
-
-                <div className="main_pages">
-                {
-                    Object.keys(pages).map((page)=>(
-                        <Link
-                                key={page}
-                                className={(isActive === page) ? 'header-link header-link--isActive' : 'header-link'}
-                                to={pages[page].url}
-                                onClick={() => { handleActive(page); }}
-                            >
-                                <Button
-                                    //onClick={handleCloseNavMenu}
-                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                <nav>
+                    <div>
+                        <Button className="button-menu" aria-describedby={id} variant="contained" onClick={handleClickMenu}>
+                            <img src={burgerMenu} className="" alt="burger menu" />
+                        </Button>
+                        <Popover
+                            id={id}
+                            open={open}
+                            anchorEl={anchorMenu}
+                            onClose={handleCloseMenu}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                        >
+                            <div className="mobile-menu">
+                        {
+                            Object.keys(pages).map((page) => (
+                                <Link
+                                    key={page}
+                                    className={(isActive === page) ? 'header-link header-link--isActive' : 'header-link'}
+                                    to={pages[page].url}
+                                    onClick={() => { handleActive(page); }}
                                 >
-                                    {pages[page].display}
-                                </Button>
-                        </Link>
-                    ))
-                }
-                <div className="header-items">
-                    {(userToken)&&<Cart cartManager={cartManager} currentItems = {currentItems}/>}
-                    <LoginUser/>
-                </div>
-                </div>
-            </nav>
-                
+                                    <Button
+                                        sx={{ my: 2, color: 'white', display: 'block' }}
+                                    >
+                                        {pages[page].display}
+                                    </Button>
+                                </Link>
+                            ))
+                        }
+                        <div className="header-items">
+                            {(userToken) && <Cart cartManager={cartManager} currentItems={currentItems} />}
+                            <LoginUser />
+                        </div>
+                    </div>
+                        </Popover>
+                    </div>
+
+                    <div className="main_pages">
+                        {
+                            Object.keys(pages).map((page) => (
+                                <Link
+                                    key={page}
+                                    className={(isActive === page) ? 'header-link header-link--isActive' : 'header-link'}
+                                    to={pages[page].url}
+                                    onClick={() => { handleActive(page); }}
+                                >
+                                    <Button
+                                        sx={{ my: 2, color: 'white', display: 'block' }}
+                                    >
+                                        {pages[page].display}
+                                    </Button>
+                                </Link>
+                            ))
+                        }
+                        <div className="header-items">
+                            {(userToken) && <Cart cartManager={cartManager} currentItems={currentItems} />}
+                            <LoginUser />
+                        </div>
+                    </div>
+                </nav>
+
             </Box>
         </header>
-   );
+    );
 };
 
 Header.propTypes = {
