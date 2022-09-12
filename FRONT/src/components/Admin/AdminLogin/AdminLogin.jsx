3b42458e19f516563933328273a651ef48ef9d11
kertesz-corentin <kertesz.corentin@hotmail.com>
2022-09-12 18:49:50 +0200

    fix: admin responsive and info

--- FRONT/src/components/Admin/AdminLogin/AdminLogin.jsx
+++ FRONT/src/components/Admin/AdminLogin/AdminLogin.jsx
@@ -10,18 +10,19 @@ import AlertMessage from '../../Front-Office/Reusable/AlertMessage/AlertMessage'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useState } from 'react';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
-import { Grid,Link } from '@mui/material';
+import { Grid, Link } from '@mui/material';
 import { useNavigate } from 'react-router';
 import api from '../../../requests/index';
 
 import './adminlogin.scss';
 
 const theme = createTheme();
- export default function SignIn() {
+export default function SignIn() {
     const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState(false);
 
     const [alertMessage, setAlertMessage] = useState();
+    const [severity, setSeverity] = React.useState();
 
     const onToggleOpen = () => {
         setIsOpen(!isOpen)
@@ -35,8 +36,11 @@ const theme = createTheme();
         try {
             const response = await api.login(email, password);
             console.log(response);
-            if(response.status === 200) {
+            if (response.status === 200) {
                 navigate('/admin/home');
+            } else {
+                setSeverity("error");
+                setAlertMessage(`email ou mot de passe incorrect`);
             }
             return response
         }
@@ -48,86 +52,86 @@ const theme = createTheme();
 
     const [anchorEl, setAnchorEl] = React.useState(null);
     const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+        setAnchorEl(event.currentTarget);
     };
     const handleClose = () => {
-    setAnchorEl(null);
+        setAnchorEl(null);
     };
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
 
 
-  return (
+    return (
 
-    <div className="admin-login">
+        <div className="admin-login">
             <ThemeProvider theme={theme}>
                 <Container component="main" maxWidth="xs">
                     <CssBaseline />
                     <Box
-                    sx={{
-                        marginTop: 1,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
+                        sx={{
+                            marginTop: 1,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
                     >
-                    <Avatar
-                    sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <SupervisorAccountIcon/>
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Se connecter
-                    </Typography>
-                    {alertMessage && (
-                        <AlertMessage
-                            message={alertMessage}
-                        >
-                        </AlertMessage>
-                    )}
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Adresse email"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                        />
-                        <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Mot de passe"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                        />
-                {/* Ici voir pour rendre disablle le bouton se connecter si les deux input ne sont pas remplies */}
-                        <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        className="admin-login-submit"
-                        sx={{ mt: 1, mb: 2 }}
-                        >
-                        Se connecter
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                Mot de passe oublié
-                                </Link>
+                        <Avatar
+                            sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <SupervisorAccountIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Se connecter
+                        </Typography>
+                        {alertMessage && (
+                            <AlertMessage
+                                message={alertMessage}
+                            >
+                            </AlertMessage>
+                        )}
+                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Adresse email"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Mot de passe"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            {/* Ici voir pour rendre disablle le bouton se connecter si les deux input ne sont pas remplies */}
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                className="admin-login-submit"
+                                sx={{ mt: 1, mb: 2 }}
+                            >
+                                Se connecter
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Mot de passe oublié
+                                    </Link>
+                                </Grid>
                             </Grid>
-                        </Grid>
-                    </Box>
+                        </Box>
                     </Box>
                 </Container>
             </ThemeProvider>
-    </div>
-  );
+        </div>
+    );
 }
 
 
